4cd64a38d3fdc3a2b141dd2e2ca615e7f93f8ac0
Sergey Lagunov <slim.setoh@gmail.com>
2018-07-06 17:59:55 +0300

    Задание выполнено с начала из-за проблем со второй папкой Задание #3

---
+++ backend/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github/ComputePractice2018/photohosting/backend/utils"
+)
+
+func main() {
+	var name := flag.String("name", "slimz", "имя для приветствия")
+	flag.Parse()
+	fmt.Println(utils.GetHelloWorldString(*name))
+}
