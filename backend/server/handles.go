0108c8e6ddb71787c79e753173aab564c3f0a7ca
Sergey Lagunov <slim.setoh@gmail.com>
2018-07-08 00:19:01 +0300

    Добавлен парсинг данных из JSON по POST Задача #5

--- backend/server/handles.go
+++ backend/server/handles.go
@@ -6,16 +6,34 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/ComputePractice/photohosting/backend/data"
+	"github.com/ComputePractice2018/photohosting/backend/data"
 )
+
+//ProfileHandler обрабатывает обрабатывает все запросы к /api/photohosting/profile
+func ProfileHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		GetProfile(w, r)
+		return
+	}
+	if r.Method == "POST" {
+		AddProfile(w, r)
+		return
+	}
+
+	message := fmt.Sprintf("Method %s is not allowed", r.Method)
+	http.Error(w, message, http.StatusMethodNotAllowed)
+	log.Println(message)
+
+}
 
 //GetProfile обрабатывает запросы на получение списка профилей
 func GetProfile(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Request method: %s", r.Method)
 	binaryData, err := json.Marshal(data.ProfileList)
 	if err != nil {
-		w.Header().Add("Content-Type", "plain/text; charset=utf-8")
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "JSON marshaling error: %v", err)
+		message := fmt.Sprintf("JSON marshaling error: %v", err)
+		http.Error(w, message, http.StatusInternalServerError)
+		log.Println(message)
 		return
 	}
 
@@ -24,6 +42,22 @@ func GetProfile(w http.ResponseWriter, r *http.Request) {
 
 	_, err = w.Write(binaryData)
 	if err != nil {
-		log.Printf("Hadler Write Error: %v", err)
+		message := fmt.Sprintf("JSON marshaling error: %v", err)
+		http.Error(w, message, http.StatusInternalServerError)
+		log.Println(message)
 	}
+}
+
+//AddProfile обрабатывает POST запрос
+func AddProfile(w http.ResponseWriter, r *http.Request) {
+	var profile data.Profile
+	err := json.NewDecoder(r.Body).Decode(&profile)
+	if err != nil {
+		message := fmt.Sprintf("Unable to decode POST data: %v", err)
+		http.Error(w, message, http.StatusUnsupportedMediaType)
+		log.Printf(message)
+		return
+	}
+	log.Printf("%+v", profile)
+	w.WriteHeader(http.StatusCreated)
 }
