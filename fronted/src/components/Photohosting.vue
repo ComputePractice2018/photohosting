2acb3878abe102bb57f7222fdc0ff3b8742d6911
ErwinPie <sanek160697@mail.ru>
2018-07-09 15:47:36 +0300

    Убрано всё лишнее из проекта Задача 5

---
+++ fronted/src/components/Photohosting.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="Photohosting">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Photohosting',
+  props: {
+    title: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
