8c51969161d6ccb2a8a45c14bfc3949d78be85e9
Sergey Lagunov <slim.setoh@gmail.com>
2018-07-09 23:56:02 +0300

    Протестировать и отрефакторить сервер Задача #11

--- backend/server/headles.go
+++ backend/server/headles.go
@@ -12,164 +12,179 @@ import (
 )
 
 //GetProfiles обрабатывает запросы на получение списка профилей
-func GetProfiles(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "application/json; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	err := json.NewEncoder(w).Encode(data.GetProfiles())
-	if err != nil {
-		message := fmt.Sprintf("Unable to encode data: %v", err)
-		http.Error(w, message, http.StatusInternalServerError)
-		log.Println(message)
-		return
+func GetProfiles(cl data.Editable) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		err := json.NewEncoder(w).Encode(cl.GetProfiles())
+		if err != nil {
+			message := fmt.Sprintf("Unable to encode data: %v", err)
+			http.Error(w, message, http.StatusInternalServerError)
+			log.Println(message)
+			return
+		}
 	}
 }
 
 //AddProfile обрабатывает POST запрос для профиля
-func AddProfile(w http.ResponseWriter, r *http.Request) {
-	var profile data.Profile
-	err := json.NewDecoder(r.Body).Decode(&profile)
-	if err != nil {
-		message := fmt.Sprintf("Unable to decode POST data: %v", err)
-		http.Error(w, message, http.StatusUnsupportedMediaType)
-		log.Println(message)
-		return
+func AddProfile(cl data.Editable) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var profile data.Profile
+		err := json.NewDecoder(r.Body).Decode(&profile)
+		if err != nil {
+			message := fmt.Sprintf("Unable to decode POST data: %v", err)
+			http.Error(w, message, http.StatusUnsupportedMediaType)
+			log.Println(message)
+			return
+		}
+		id := cl.AddProfile(profile)
+		w.Header().Add("Location", r.URL.String()+"/"+strconv.Itoa(id))
+		w.WriteHeader(http.StatusCreated)
 	}
-	id := data.AddProfile(profile)
-	w.Header().Add("Location", r.URL.String()+"/"+strconv.Itoa(id))
-	w.WriteHeader(http.StatusCreated)
 }
 
 //EditProfile обрабатывает PUT запрос для профиля
-func EditProfile(w http.ResponseWriter, r *http.Request) {
-	var profile data.Profile
-	err := json.NewDecoder(r.Body).Decode(&profile)
-	if err != nil {
-		message := fmt.Sprintf("Unable to decode PUT data: %v", err)
-		http.Error(w, message, http.StatusUnsupportedMediaType)
-		log.Println(message)
-		return
-	}
-	vars := mux.Vars(r)
-	id, err := strconv.Atoi(vars["id"])
-	if err != nil {
-		message := fmt.Sprintf("Incorrect ID: %v", err)
-		http.Error(w, message, http.StatusBadRequest)
-		log.Println(message)
-		return
-	}
+func EditProfile(cl data.Editable) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var profile data.Profile
+		err := json.NewDecoder(r.Body).Decode(&profile)
+		if err != nil {
+			message := fmt.Sprintf("Unable to decode PUT data: %v", err)
+			http.Error(w, message, http.StatusUnsupportedMediaType)
+			log.Println(message)
+			return
+		}
+		vars := mux.Vars(r)
+		id, err := strconv.Atoi(vars["id"])
+		if err != nil {
+			message := fmt.Sprintf("Incorrect ID: %v", err)
+			http.Error(w, message, http.StatusBadRequest)
+			log.Println(message)
+			return
+		}
 
-	err = data.EditProfile(profile, id)
-	if err != nil {
-		message := fmt.Sprintf("Incorrect ID: %v", err)
-		http.Error(w, message, http.StatusBadRequest)
-		log.Println(message)
-		return
-	}
-	w.Header().Add("Location", r.URL.String())
-	w.WriteHeader(http.StatusAccepted)
+		err = cl.EditProfile(profile, id)
+		if err != nil {
+			message := fmt.Sprintf("Incorrect ID: %v", err)
+			http.Error(w, message, http.StatusBadRequest)
+			log.Println(message)
+			return
+		}
+		w.Header().Add("Location", r.URL.String())
+		w.WriteHeader(http.StatusAccepted)
 
+	}
 }
 
 // DeleteProfile обрабатывает DELETE запрос для профиля
+func DeleteProfile(cl data.Editable) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		id, err := strconv.Atoi(vars["id"])
+		if err != nil {
+			message := fmt.Sprintf("Incorrect ID: %v", err)
+			http.Error(w, message, http.StatusBadRequest)
+			log.Println(message)
+			return
+		}
 
-func DeleteProfile(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id, err := strconv.Atoi(vars["id"])
-	if err != nil {
-		message := fmt.Sprintf("Incorrect ID: %v", err)
-		http.Error(w, message, http.StatusBadRequest)
-		log.Println(message)
-		return
+		err = cl.RemoveProfile(id)
+		if err != nil {
+			message := fmt.Sprintf("Incorrect ID: %v", err)
+			http.Error(w, message, http.StatusBadRequest)
+			log.Println(message)
+			return
+		}
+		w.Header().Add("Location", r.URL.String())
+		w.WriteHeader(http.StatusNoContent)
 	}
-
-	err = data.RemoveProfile(id)
-	if err != nil {
-		message := fmt.Sprintf("Incorrect ID: %v", err)
-		http.Error(w, message, http.StatusBadRequest)
-		log.Println(message)
-		return
-	}
-	w.Header().Add("Location", r.URL.String())
-	w.WriteHeader(http.StatusNoContent)
 }
 
 //GetPhotos обрабатывает запросы на получение списка фотографий
-func GetPhotos(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "application/json; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	err := json.NewEncoder(w).Encode(data.GetPhotos())
-	if err != nil {
-		message := fmt.Sprintf("Unable to encode data: %v", err)
-		http.Error(w, message, http.StatusInternalServerError)
-		log.Println(message)
-		return
+func GetPhotos(cl data.Editable) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		err := json.NewEncoder(w).Encode(cl.GetPhotos())
+		if err != nil {
+			message := fmt.Sprintf("Unable to encode data: %v", err)
+			http.Error(w, message, http.StatusInternalServerError)
+			log.Println(message)
+			return
+		}
 	}
 }
 
 //AddPhoto обрабатывает POST запрос для фотографий
-func AddPhoto(w http.ResponseWriter, r *http.Request) {
-	var photo data.Photo
-	err := json.NewDecoder(r.Body).Decode(&photo)
-	if err != nil {
-		message := fmt.Sprintf("Unable to decode POST data: %v", err)
-		http.Error(w, message, http.StatusUnsupportedMediaType)
-		log.Println(message)
-		return
+func AddPhoto(cl data.Editable) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var photo data.Photo
+		err := json.NewDecoder(r.Body).Decode(&photo)
+		if err != nil {
+			message := fmt.Sprintf("Unable to decode POST data: %v", err)
+			http.Error(w, message, http.StatusUnsupportedMediaType)
+			log.Println(message)
+			return
+		}
+		id := cl.AddPhoto(photo)
+		w.Header().Add("Location", r.URL.String()+"/"+strconv.Itoa(id))
+		w.WriteHeader(http.StatusCreated)
 	}
-	id := data.AddPhoto(photo)
-	w.Header().Add("Location", r.URL.String()+"/"+strconv.Itoa(id))
-	w.WriteHeader(http.StatusCreated)
 }
 
 //EditPhoto обрабатывает PUT запрос для фотографий
-func EditPhoto(w http.ResponseWriter, r *http.Request) {
-	var photo data.Photo
-	err := json.NewDecoder(r.Body).Decode(&photo)
-	if err != nil {
-		message := fmt.Sprintf("Unable to decode PUT data: %v", err)
-		http.Error(w, message, http.StatusUnsupportedMediaType)
-		log.Println(message)
-		return
-	}
-	vars := mux.Vars(r)
-	id, err := strconv.Atoi(vars["id"])
-	if err != nil {
-		message := fmt.Sprintf("Incorrect ID: %v", err)
-		http.Error(w, message, http.StatusBadRequest)
-		log.Println(message)
-		return
-	}
+func EditPhoto(cl data.Editable) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var photo data.Photo
+		err := json.NewDecoder(r.Body).Decode(&photo)
+		if err != nil {
+			message := fmt.Sprintf("Unable to decode PUT data: %v", err)
+			http.Error(w, message, http.StatusUnsupportedMediaType)
+			log.Println(message)
+			return
+		}
+		vars := mux.Vars(r)
+		id, err := strconv.Atoi(vars["id"])
+		if err != nil {
+			message := fmt.Sprintf("Incorrect ID: %v", err)
+			http.Error(w, message, http.StatusBadRequest)
+			log.Println(message)
+			return
+		}
 
-	err = data.EditPhoto(photo, id)
-	if err != nil {
-		message := fmt.Sprintf("Incorrect ID: %v", err)
-		http.Error(w, message, http.StatusBadRequest)
-		log.Println(message)
-		return
-	}
-	w.Header().Add("Location", r.URL.String())
-	w.WriteHeader(http.StatusAccepted)
+		err = cl.EditPhoto(photo, id)
+		if err != nil {
+			message := fmt.Sprintf("Incorrect ID: %v", err)
+			http.Error(w, message, http.StatusBadRequest)
+			log.Println(message)
+			return
+		}
+		w.Header().Add("Location", r.URL.String())
+		w.WriteHeader(http.StatusAccepted)
 
+	}
 }
 
 // DeletePhoto обрабатывает DELETE запрос для фотографий
-func DeletePhoto(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id, err := strconv.Atoi(vars["id"])
-	if err != nil {
-		message := fmt.Sprintf("Incorrect ID: %v", err)
-		http.Error(w, message, http.StatusBadRequest)
-		log.Println(message)
-		return
-	}
+func DeletePhoto(cl data.Editable) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		id, err := strconv.Atoi(vars["id"])
+		if err != nil {
+			message := fmt.Sprintf("Incorrect ID: %v", err)
+			http.Error(w, message, http.StatusBadRequest)
+			log.Println(message)
+			return
+		}
 
-	err = data.RemovePhoto(id)
-	if err != nil {
-		message := fmt.Sprintf("Incorrect ID: %v", err)
-		http.Error(w, message, http.StatusBadRequest)
-		log.Println(message)
-		return
+		err = cl.RemovePhoto(id)
+		if err != nil {
+			message := fmt.Sprintf("Incorrect ID: %v", err)
+			http.Error(w, message, http.StatusBadRequest)
+			log.Println(message)
+			return
+		}
+		w.Header().Add("Location", r.URL.String())
+		w.WriteHeader(http.StatusNoContent)
 	}
-	w.Header().Add("Location", r.URL.String())
-	w.WriteHeader(http.StatusNoContent)
 }
