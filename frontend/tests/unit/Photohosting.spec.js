e7a51f70d5f7bd3644b6d0b64dbd80088444c8af
ErwinPie <sanek160697@mail.ru>
2018-07-11 14:58:00 +0300

    1

--- frontend/tests/unit/Photohosting.spec.js
+++ frontend/tests/unit/Photohosting.spec.js
@@ -4,18 +4,19 @@ import Photohosting from '@/components/Photohosting.vue'
 
 describe('Photohosting.vue', () => {
   it('renders props.title when passed', () => {
-    const title = 'Название'
+    const title = 'Name'
     const wrapper = shallowMount(Photohosting, {
       propsData: { title }
     })
     expect(wrapper.text()).to.include(title)
   })
-  it('renders Имя', () => {
+  it('renders titles', () => {
     const wrapper = shallowMount(Photohosting, {})
-    expect(wrapper.text()).to.include('Name')
-    expect(wrapper.text()).to.include('Nickname')
-    expect(wrapper.text()).to.include('Email')
+    expect(wrapper.text()).to.include('name')
+    expect(wrapper.text()).to.include('nickname')
+    expect(wrapper.text()).to.include('email')
     expect(wrapper.text()).to.include('description')
     expect(wrapper.text()).to.include('birthday')
     expect(wrapper.text()).to.include('photos')
+  })
 })
